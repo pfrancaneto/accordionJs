e7fef00cf8ff2445f41a5ec8190b6d4191743973
Pedro França <pfranca.neto@gmail.com>
2023-12-16 09:42:46 -0300

    instalando fonts + ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Faq from "./components/Faq";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Faq />
     </>
   );
 }
